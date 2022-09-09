f6277093845642929b6b7bb02af739e2ac0dee1e
Metails <dengjianhua2397462613@gmail.com>
2022-09-08 23:41:42 -0400

    Added task B

--- main.cpp
+++ main.cpp
@@ -8,7 +8,8 @@
 int main()
 {
 
-	std::cout << "East basin storage: " << get_east_storage("05/20/2018") << "billion gallons";
-
+	std::cout << "East basin storage: " << get_east_storage("05/20/2018") << " billion gallons " << std::endl;
+	std::cout << "Minimum East basin storage: " << get_min_east() << " billion gallons " << std::endl;
+	std::cout << "Maximum East basin storage: " << get_max_east() << " billion gallons " << std::endl;
 	return 0;
 }//end main
--- reservoir.cpp
+++ reservoir.cpp
@@ -5,6 +5,8 @@
 #include <climits>
 #include "reservoir.h"
 
+//---------------------------------------------------------------------------------
+
 double get_east_storage(std::string inputDate) {
 
 	//read tsv file, declaring fin as the input file stream object
@@ -40,3 +42,93 @@ double get_east_storage(std::string inputDate) {
 	return targetValue;
 
 }//end get_east_storage function
+
+//---------------------------------------------------------------------------------
+
+double get_min_east() {
+	
+	//Copied and pasted previous code of reading tsv file
+	//read tsv file, declaring fin as the input file stream object
+	std::ifstream fin("Current_Reservoir_Levels.tsv");
+
+	//Conditions for failing to read the tsv file
+	if (fin.fail()) {
+		std::cerr << "File Cannot be opened for reading." << std::endl;
+		exit(1);
+	}//end condition
+
+	std::string junk;
+	getline(fin, junk);//read the first line within tsv file as the first line will only has headers
+
+	std::string date;
+	double eastSt, eastEl, westSt, westEl, smallest;
+	bool secondLine = true;
+
+	//Loop through by the headings
+	while (fin >> date >> eastSt >> eastEl >> westSt >> westEl) {
+
+		fin.ignore(INT_MAX, '\n');
+
+		//This checks if it's iterating the first loop, if so, set samllest to the first value so it could move on to the next loop and compare to the next number.
+		if (secondLine) {
+			smallest = eastSt;
+			secondLine = false;
+		}//end condition
+
+		//Comparing numbers
+		if (smallest > eastSt && !secondLine) {
+			smallest = eastSt;
+		}//end condition
+
+
+	}//end of while loop
+
+	fin.close();
+
+	return smallest;
+
+}//end get)min_east function
+
+
+//---------------------------------------------------------------------------------
+
+
+double get_max_east() {
+	
+	//Copied and pasted previous code of reading tsv file
+	//read tsv file, declaring fin as the input file stream object
+	std::ifstream fin("Current_Reservoir_Levels.tsv");
+
+	//Conditions for failing to read the tsv file
+	if (fin.fail()) {
+		std::cerr << "File Cannot be opened for reading." << std::endl;
+		exit(1);
+	}//end condition
+
+	std::string junk;
+	getline(fin, junk);//read the first line within tsv file as the first line will only has headers
+
+	std::string date;
+	double eastSt, eastEl, westSt, westEl, largest;
+	bool secondLine = true;
+
+	//Loop through by the headings
+	while (fin >> date >> eastSt >> eastEl >> westSt >> westEl) {
+
+		fin.ignore(INT_MAX, '\n');
+
+		//This checks if it's iterating the first loop, if so, set samllest to the first value so it could move on to the next loop and compare to the next number.
+		if (secondLine) {
+			largest = eastSt;
+			secondLine = false;
+		}//end condition
+
+		//compare numbers
+		if (largest < eastSt && !secondLine) {
+			largest = eastSt;
+		}//end condition
+	
+	}//end while loop
+
+	return largest;
+}//end get_max_east function
