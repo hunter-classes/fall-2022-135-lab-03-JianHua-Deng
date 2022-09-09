3f1aa2009b31a646db27852367383ccc6ae84abd
Metails <dengjianhua2397462613@gmail.com>
2022-09-09 15:17:39 -0400

    Added task C function and test cases

--- main.cpp
+++ main.cpp
@@ -4,9 +4,26 @@
 
 int main()
 {
-
+	//test cases for get_east_storage
+	std::cout << std::endl;
 	std::cout << "East basin storage: " << get_east_storage("05/20/2018") << " billion gallons " << std::endl;
+	
+	//test cases for get_min_east
+	std::cout << std::endl;
 	std::cout << "Minimum East basin storage: " << get_min_east() << " billion gallons " << std::endl;
+	
+	//test cases for get_max_east
 	std::cout << "Maximum East basin storage: " << get_max_east() << " billion gallons " << std::endl;
+	
+	//test cases for compare_basins, however there aren't cases for East being greater or both being equal.
+	//Due to the reason that most data shows west has the highest basin
+	std::cout << std::endl;
+	std::cout << "The one that consists of the highest basin is " << compare_basins("09/13/2018") << std::endl;
+	std::cout << "The one that consists of the highest basin is " << compare_basins("09/14/2018") << std::endl;
+	std::cout << "The one that consists of the highest basin is " << compare_basins("09/15/2018") << std::endl;
+	std::cout << "The one that consists of the highest basin is " << compare_basins("09/16/2018") << std::endl;
+	std::cout << "The one that consists of the highest basin is " << compare_basins("09/17/2018") << std::endl;
+
+
 	return 0;
 }//end main
--- reservoir.cpp
+++ reservoir.cpp
@@ -131,4 +131,61 @@ double get_max_east() {
 	}//end while loop
 
 	return largest;
+
 }//end get_max_east function
+
+
+//---------------------------------------------------------------------------------
+
+std::string compare_basins(std::string inputDate) {
+
+
+	//Copied and pasted previous code of reading tsv file
+	//read tsv file, declaring fin as the input file stream object
+	std::ifstream fin("Current_Reservoir_Levels.tsv");
+
+	//Conditions for failing to read the tsv file
+	if (fin.fail()) {
+		std::cerr << "File Cannot be opened for reading." << std::endl;
+		exit(1);
+	}//end condition
+
+	std::string junk;
+	getline(fin, junk);//read the first line within tsv file as the first line will only has headers
+
+	//initializing variables
+	std::string date, result;
+	double eastSt, eastEl, westSt, westEl;
+	bool secondLine = true;
+
+	//Loop through by the headings
+	while (fin >> date >> eastSt >> eastEl >> westSt >> westEl) {
+
+		fin.ignore(INT_MAX, '\n');
+
+		//when the line matches the date of the inputDate in parameter
+		if (inputDate.compare(date) == 0) {
+			//if east elevation is higher
+			if (eastEl > westEl) {
+
+				result = "East";
+
+			}else if (eastEl < westEl) { //if west elevation i s higher
+
+				result = "West";
+
+			}else { //if all above condition doesn't meet, it has to be equal
+
+				result = "Equal";
+
+			}//end condition
+
+		}//end if condition
+
+	}//end while loop
+
+	fin.close();
+
+	return result;
+
+}//end of compare_basins function
