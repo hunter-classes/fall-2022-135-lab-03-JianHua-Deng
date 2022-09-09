ff9f2d1e3fc7a5d30288a71a3cd8a1d6c39b9e38
Metails <dengjianhua2397462613@gmail.com>
2022-09-09 15:56:54 -0400

    Updated test cases

--- main.cpp
+++ main.cpp
@@ -15,14 +15,16 @@ int main()
 	//test cases for get_max_east
 	std::cout << "Maximum East basin storage: " << get_max_east() << " billion gallons " << std::endl;
 	
-	//test cases for compare_basins, however there aren't cases for East being greater or both being equal.
-	//Due to the reason that most data shows west has the highest basin
+	//test cases for compare_basins, however I couldn't find cases for both basin being equals
+	//West
 	std::cout << std::endl;
 	std::cout << "The one that consists of the highest basin is " << compare_basins("09/13/2018") << std::endl;
 	std::cout << "The one that consists of the highest basin is " << compare_basins("09/14/2018") << std::endl;
-	std::cout << "The one that consists of the highest basin is " << compare_basins("09/15/2018") << std::endl;
-	std::cout << "The one that consists of the highest basin is " << compare_basins("09/16/2018") << std::endl;
-	std::cout << "The one that consists of the highest basin is " << compare_basins("09/17/2018") << std::endl;
+
+	//East
+	std::cout << "The one that consists of the highest basin is " << compare_basins("01/11/2018") << std::endl;
+	std::cout << "The one that consists of the highest basin is " << compare_basins("01/10/2018") << std::endl;
+	
 
 
 	return 0;
